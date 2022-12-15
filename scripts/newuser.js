feb4b32caa15471f9e4917ffadf4c59e5ce663f8
Richard Perez <richard.floresperez0@gmail.com>
2022-12-15 11:10:33 -0500

    Added functionality to add a new user to the users API. Again, it shows the new user/data in the API, but the catch method executes

---
+++ scripts/newuser.js
@@ -0,0 +1,39 @@
+"use strict";
+window.onload = () => {
+    document.getElementById("addUser").onclick = () => {
+        const actualName = document.getElementById("actualNameInput").value;
+        const username = document.getElementById("userNameInput").value;
+        const password = document.getElementById("passwordInput").value;
+        const confirmpw = document.getElementById("confirmPassword").value;
+        const anyEmptyValues = Boolean(actualName) && Boolean(username) && Boolean(password) && Boolean(confirmpw);
+        const error = document.getElementById("errorMessage");
+        const success = document.getElementById("successMessage");
+
+        if (anyEmptyValues && (password === confirmpw)) {
+            let newUser = {
+                name: actualName,
+                username: username,
+                password: password
+            }
+            fetch("http://localhost:8083/api/users", {
+                method: "POST",
+                body: JSON.stringify(newUser),
+                headers: {
+                    "Content-type":
+                        "application/json; charset=UTF-8"
+                }
+            }).then(response => response.json())
+                .then(results => {
+                    success.innerHTML = "LET's GO!";
+                    error.innerHTML = "";
+                })
+                .catch(error => {
+                    success.innerHTML = "";
+                    error.innerHTML = "Unexpected error";
+                })
+        } else {
+            success.innerHTML = "";
+            error.innerHTML = "Either you gave me empty values or passwords don't match. Not me, it's YOU"
+        }
+    }
+}
